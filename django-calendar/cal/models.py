b23ba8101b26cc2b58843ca317a5dad834582e95
OlegEgoism <olegpustovalov220@gmail.com>
2023-08-24 15:43:19 +0300

    Доработка по пользователю.

--- django-calendar/cal/models.py
+++ django-calendar/cal/models.py
@@ -1,18 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 
 
 class Event(models.Model):
-    """Календарь учителя"""
+    """Календарь"""
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     start_time = models.DateTimeField(verbose_name='Время начала')
     end_time = models.DateTimeField(verbose_name='Время окончания')
-    user = models.ForeignKey('User', verbose_name='ФИО', related_name='user_event', on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey('User', verbose_name='ФИО', related_name='user_event', on_delete=models.CASCADE,
+                             blank=True, null=True)
 
     class Meta:
-        verbose_name = 'Календарь учителя'
-        verbose_name_plural = 'Календарь учителя'
+        verbose_name = 'Календарь'
+        verbose_name_plural = 'Календарь'
         ordering = 'start_time',
 
     @property
@@ -20,14 +22,23 @@ class Event(models.Model):
         url = reverse('cal:event_edit', args=(self.id,))
         return f'<a href="{url}"> {self.start_time.__format__("%H:%M")} - {self.user}</a>'  # <hr>'
 
+    def clean(self):
+        """Проверка даты"""
+        if self.end_time <= self.start_time:
+            raise ValidationError('Дата окончания должна быть позже даты начала.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
 
 class User(models.Model):
-    """Ученик"""
+    """Пользователь"""
     name = models.CharField(verbose_name='ФИО', max_length=200)
 
     class Meta:
-        verbose_name = 'Ученик'
-        verbose_name_plural = 'Ученик'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователь'
         ordering = 'name',
 
     def __str__(self):
--- django-calendar/cal/views.py
+++ django-calendar/cal/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, date
 from django.db.models import Count
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
@@ -63,15 +63,13 @@ def next_month(d):
 
 
 def event(request, id=None):
-    """Добавление мероприятий"""
-    if id:
-        instance = get_object_or_404(Event, id=id)
-    else:
-        instance = Event()
+    """Просмотр и редактирование мероприятия"""
+    instance = get_object_or_404(Event, id=id) if id else Event()
     form = EventForm(request.POST or None, instance=instance)
     if request.POST and form.is_valid():
         form.save()
         return HttpResponseRedirect(reverse('cal:calendar'))
+
     context = {
         'form': form,
         'event_del': instance,
@@ -87,24 +85,18 @@ class EventMemberDeleteView(DeleteView):
 
 
 def user_list(request):
+    """Список пользователей"""
     users_with_event_count = User.objects.annotate(event_count=Count('user_event'))
+    total_event_count = Event.objects.count()
     context = {
-        'users_with_event_count': users_with_event_count
+        'users_with_event_count': users_with_event_count,
+        'total_event_count': total_event_count
     }
     return render(request, 'cal/user_list.html', context)
 
 
-def user_detail(request, user_id):
-    """Информмация о пользователе"""
-    user = User.objects.get(pk=user_id)
-    context = {
-        'user': user
-    }
-    return render(request, 'cal/user_detail.html', context)
-
-
 def add_user(request):
-    """Добовляение пользователей"""
+    """Добовляение пользователя"""
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         if user_form.is_valid():
@@ -116,3 +108,17 @@ def add_user(request):
         'user_form': user_form,
     }
     return render(request, 'cal/add_user.html', context=context)
+
+
+def user_detail(request, user_id):
+    """Информмация о пользователе"""
+    user = User.objects.get(pk=user_id)
+    all_event = user.user_event.all()
+    if request.method == 'POST':
+        user.delete()
+        return redirect('cal:user_list')
+    context = {
+        'user': user,
+        'all_event': all_event
+    }
+    return render(request, 'cal/user_detail.html', context)
